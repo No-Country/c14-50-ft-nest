dbe15f28ae7bf90169a5a9d815167847d0de6baf
Nahuel <Nahugc258@gmail.com>
2023-10-05 14:47:45 -0300

    Add form validate

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,31 +1,113 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRef } from 'react';
+import { useForm } from "react-hook-form";
+import RegisterSchema, { options } from './RegisterSchema';
+
+type FormData = {
+  name: string
+  lastName: string
+  dni: string
+  email: string
+  birthdate: string
+  phoneNumber: string
+  password: string
+  confirmPassword: string
+  insurance: string
+}
 
 const RegisterForm = () => {
+  const date = useRef<HTMLInputElement>(null)
+  const {
+    register,
+    unregister,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(RegisterSchema) })
+  const submitData = (data: FormData) => {
+    console.log(data)
+    reset()
+  }
+
+  const handleChange = () => {
+    unregister('birthdate')
+    if(date.current !== null){
+      const birthdate = new Date(date.current.value)
+      register('birthdate', {value: birthdate.toISOString()})
+    }
+  }
+
   return (
-    <form className="flex gap-2 flex-col text-zinc-700 w-6/12">
-      <label>Nombre</label>
-      <input type="text" placeholder="Nombre"/>
+    <form className="flex gap-3 flex-col text-black "
+      onSubmit={handleSubmit(submitData)}
+    >
+      <div className='flex justify-between'>
+        <label>Nombre</label>
+        <input type="text" {...register('name')} placeholder="Nombre"/>
+      </div>
+      {errors.name && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.name.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Apellido</label>
-      <input type="text" placeholder="Apellido"/>
+      <input type="text" {...register('lastName')} placeholder="Apellido"/>
+      </div>
+      {errors.lastName && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.lastName.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>DNI</label>
-      <input type="number" placeholder="DNI"/>
+      <input type="number" {...register('dni')} placeholder="DNI"/>
+      </div>
+      {errors.dni && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.dni.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Email</label>
-      <input type="email" placeholder="Email"/>
+      <input type="email" {...register('email')} placeholder="Email"/>
+      </div>
+      {errors.email && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.email.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Obra Social</label>
-      <select>
-        <option></option>
-        <option>Obra Social 1</option>
-        <option>Obra Social 2</option>
-        <option>Obra Social 3</option>
-        <option>Obra Social 4</option>
+      <select {...register('insurance')}>
+        <option value=''>* Seleccione Obra Social *</option>
+        {options.map(option => <option key={option} value={option}>{option}</option>)}
       </select>
+      </div>
+      {errors.insurance && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.insurance.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Fecha de Nacimiento</label>
-      <input type="date" placeholder="Fecha de nacimiento"/>
+      <input type="date" ref={date} onChange={handleChange}/>
+      </div>
+      {errors.birthdate && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.birthdate.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Número de Teléfono</label>
-      <input type="number" placeholder="Número de teléfono"/>
+      <input type="number" {...register('phoneNumber')} placeholder="Número de teléfono"/>
+      </div>
+      {errors.phoneNumber && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.phoneNumber.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Contraseña</label>
-      <input type="password" placeholder="Contraseña"/>
+      <input type="password" {...register('password')} placeholder="Contraseña"/>
+      </div>
+      {errors.password && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm  '>{errors.password.message}</span>
+      )}
+      <div className='flex justify-between'>
       <label>Confirmar Contraseña</label>
-      <input type="password" placeholder="Confirmar contraseña"/>
+      <input type="password" {...register('confirmPassword')} placeholder="Confirmar contraseña"/>
+      </div>
+      {errors.confirmPassword && (
+        <span className='bg-red-200 text-red-600 px-4 rounded-sm'>{errors.confirmPassword.message}</span>
+      )}
       <button type="submit">Registrar</button>
     </form>
   )
---
+++ frontend/src/components/RegisterSchema.ts
@@ -0,0 +1,37 @@
+import z, { ZodType } from "zod";
+
+type FormData = {
+  name: string
+  lastName: string
+  dni: string
+  email: string
+  birthdate: string
+  phoneNumber: string
+  password: string
+  confirmPassword: string
+  insurance: string
+}
+
+export const options = ['Obra Social 1', 'Obra Social 2', 'Obra Social 3', 'Obra Social 4'] as const
+
+const RegisterSchema: ZodType<FormData> = z.object({
+  name: z.string().min(3, {message: 'El nombre debe tener al menos 3 caracteres'}),
+  lastName: z.string().min(4, { message: 'El apellido debe tener al menos 4 caracteres' }),
+  dni: z.string().refine((value) => value.replace(/\D/g, '').length > 7, {
+    message: 'Ingrese un DNI válido',
+  }),
+  email: z.string().min(4, { message: 'Ingrese un email válido' }).email(),
+  insurance: z.enum(options, {errorMap: () => ({message: 'Selecciona una obra social'})}),
+  birthdate: z.string().datetime(),
+  phoneNumber: z.string().refine((value) => value.replace(/\D/g, '').length > 9, {
+    message: 'Ingrese un número de telefono válido',
+  }),
+  password: z.string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
+  confirmPassword: z.string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: 'Las contraseñas no coinciden!',
+  path: ['confirmPassword'],
+})
+
+export default RegisterSchema
+
