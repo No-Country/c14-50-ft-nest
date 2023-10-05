8b3fcb387a2c3f7dc6971056ff058163f8e41a8d
Ferwin Arias <ferwin.arias@gmail.com>
2023-10-04 22:36:02 -0500

    Initial Settings

---
+++ backend_nc_project/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
