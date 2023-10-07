9f870b96c9ffdf3369d39153afb3a32303ebb12c
nicoliprogramer <imotivacionesf@gmail.com>
2023-10-07 20:53:51 -0300

    login and register endpoints created

--- backend_nc_project/src/auth/auth.controller.ts
+++ backend_nc_project/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    @Post('register')
+        async register(){
+            return this.authService.register()
+        }
+    
+    @Post('login')
+        async login(){
+            return this.authService.login()
+        }
+}
