e5e359e7a1a9d810978c65746819df4afa58bf61
nicoliprogramer <imotivacionesf@gmail.com>
2023-10-07 22:22:29 -0300

    dto incorparated for register and login

--- backend_nc_project/src/auth/auth.service.ts
+++ backend_nc_project/src/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import LoginDto from './dto/login.dto';
+import RegisterDto from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
 
-    async register(){
-        return "registered"
+    async register(registerDto: RegisterDto){
+        return registerDto
     }
 
-    async login(){
-        return "logged"
+    async login(loginDto: LoginDto){
+        return loginDto
     }
 }
