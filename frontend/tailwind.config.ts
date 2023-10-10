4146c5014b648cc1162b969aafe0eed6c7368a31
Emmanuel C <misterium273@gmail.com>
2023-10-10 10:25:18 -0400

    style: se añadio variables en tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,22 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      xs: { min: "0px", max: "767px" },
-
-      md: { min: "768px", max: "1024px" },
-      // => @media (min-width: 768px) { ... }
-
-      lg: { min: "1024px", max: "2000px" },
-      // => @media (min-width: 1024px) { ... }
-      xl: { min: "768px", max: "2000px" },
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'colorPrimary': '#158d9e',
+        'colorSecondary': '#000000',
+      },
+      fontFamily: {
+        Nunito: ["Nunito", "sans-serif"]
+      }
     },
   },
   plugins: [],
