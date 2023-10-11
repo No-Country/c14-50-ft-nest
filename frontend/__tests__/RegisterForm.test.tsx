8ace938f2cb894a9d8e71384e4a984097478eb03
Nahuel <Nahugc258@gmail.com>
2023-10-11 16:54:48 -0300

    fix dependencies

---
+++ frontend/__tests__/RegisterForm.test.tsx
@@ -0,0 +1,15 @@
+import RegisterForm from '@/components/RegisterForm'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+
+it('Shoud have a register button', () => {
+  render(<RegisterForm/>) 
+
+  const myElem = screen.getByText('Registrar')
+  expect(myElem).toBeInTheDocument()
+})
+
+// eslint-plugin-testing-library
+// eslint-plugin-jest-dom
+// @types/jest
