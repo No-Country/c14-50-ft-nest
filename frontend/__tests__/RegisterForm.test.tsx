c4c1be3babbe4cae293c363b40f078d6a4496323
PaoBasQA <125516330+PaoBasQA@users.noreply.github.com>
2023-10-11 18:48:11 -0300

    Iniciando test de registro

--- frontend/__tests__/RegisterForm.test.tsx
+++ frontend/__tests__/RegisterForm.test.tsx
@@ -1,5 +1,5 @@
 import RegisterForm from '@/components/RegisterForm'
-import '@testing-library/jest-dom'
+
 import { render, screen } from '@testing-library/react'
 
 
@@ -8,4 +8,25 @@ it('Shoud have a register button', () => {
 
   const myElem = screen.getByText('Registrar')
   expect(myElem).toBeInTheDocument()
+})
+
+it('Rellenar un Formulario', () => {
+  render(<ResgisterForm/>)
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+  expect(nameInput ).toHaveValue('John Doe');
+
+  const nameInput = screen.getByRole('textbox', { lastname: /lastname/i });
+  const emailInput = screen.getByRole('textbox', { email: /email/i });
+  const emailInput = screen.getByRole('textbox', { dni: /dni/i });
+  const submitButton = screen.getByRole('textbox', { password: /password/i });
+  const submitButton = screen.getByRole('textbox', { repeatPassword: /repeatPassword/i });
+  const submitButton = screen.getByRole('textbox', { phoneNumber: /phoneNumber/i }); 
+
+  const selectElement = getByRole('combobox'); // Obtén el select
+  userEvent.selectOptions(selectElement, getByText('Seleccione Obra Social'));
+
+  expect (Rellenar un Formulario).toHaveValue ('')
+
 })
