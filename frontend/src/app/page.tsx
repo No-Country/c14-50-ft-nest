94c93093c7519aa2be2c9844b5b8ac60d5a47e39
Emmanuel C <misterium273@gmail.com>
2023-10-14 12:04:33 -0400

    style: se añadio un punto en letra login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>App</h1>
-      <Link className="p-2 underline" href="/auth/login">Login</Link>
+      <Link className="p-2 underline" href="/auth/login">Login.</Link>
       <Link className="p-2 underline" href="/auth/register">Registrarse</Link>
     </main>
   )
